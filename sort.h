6d7846aa5582ec2f98a4a73a14fd9b08fa5e40e8
rajae <hemmanyrajae@gmail.com>
2024-05-08 00:44:21 -0700

    shell sort with gap technique

--- sort.h
+++ sort.h
@@ -26,4 +26,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void _Qsort(int *a, int low, int high, int size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void _shsort(int *a, int size, int n);
 #endif
