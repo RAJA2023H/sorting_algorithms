adca1304e0d65bc76f8a4ca9b30ea736481dd4d2
rajae <hemmanyrajae@gmail.com>
2024-05-07 23:00:06 -0700

    betty was mad

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,8 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j; int temp = 0;
+	size_t i, j;
+	int temp = 0;
 
 	if (!array || size < 2)
 		return;
@@ -22,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				print_array(array,size);
+				print_array(array, size);
 			}
 		}
 	}
